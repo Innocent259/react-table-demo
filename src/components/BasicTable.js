e3756873368bb788d3d9ec76f085ca885342f49d
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2024-01-18 20:07:11 +0200

    Add rows

--- src/components/BasicTable.js
+++ src/components/BasicTable.js
@@ -1,44 +1,45 @@
-import React, { useMemo } from 'react';
-import { useTable } from 'react-table';
-import { Columns } from './column';
-import { mockedData } from './MOCK_DATA.json'
+import React, { useMemo } from "react";
+import { useTable } from "react-table";
+import { Columns } from "./column";
+import { mockedData } from "./MOCK_DATA.json";
 
 const BasicTable = () => {
   const columns = useMemo(() => Columns, []);
-  const data = useMemo(() => mockedData, [] )
+  const data = useMemo(() => mockedData, []);
 
   const tableInstance = useTable({
-    columns, data
-  })
+    columns,
+    data,
+  });
 
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = tableInstance
-  console.log(tableInstance)
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    tableInstance;
+  console.log(tableInstance);
   return (
-    <table { ...getTableProps() }>
+    <table {...getTableProps()}>
       <thead>
-      {headerGroups.map((headerGroup) => (
-        <tr { ...headerGroup.getHeaderGroupProps() }>
-          {headerGroup.headers.map((column) => (
-            <th { ...column.getHeaderProps() }>
-              {column.render('Header')}
-            </th>
-            ))} 
-        </tr>
-      ))}    
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+            ))}
+          </tr>
+        ))}
       </thead>
-      <tbody { ...getTableBodyProps() }>
-      { rows.map((row) => {
-        prepareRow(row)
-        return (
-          <tr>
-            <td></td>
-        </tr>
-        )
-      }) }
-        
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {...row.cells.map((cell) => {
+                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
+              })}
+            </tr>
+          );
+        })}
       </tbody>
     </table>
-  )
-}
+  );
+};
 
-export default BasicTable
+export default BasicTable;
