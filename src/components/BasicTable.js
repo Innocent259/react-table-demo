b8a2825c0b126f1a8f3be08e22c74e7b6d4183a6
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2024-01-18 18:50:29 +0200

    Create a table

---
+++ src/components/BasicTable.js
@@ -0,0 +1,44 @@
+import React, { useMemo } from 'react';
+import { useTable } from 'react-table';
+import { Columns } from './column';
+import { mockedData } from './MOCK_DATA.json'
+
+const BasicTable = () => {
+  const columns = useMemo(() => Columns, []);
+  const data = useMemo(() => mockedData, [] )
+
+  const tableInstance = useTable({
+    columns, data
+  })
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = tableInstance
+  console.log(tableInstance)
+  return (
+    <table { ...getTableProps() }>
+      <thead>
+      {headerGroups.map((headerGroup) => (
+        <tr { ...headerGroup.getHeaderGroupProps() }>
+          {headerGroup.headers.map((column) => (
+            <th { ...column.getHeaderProps() }>
+              {column.render('Header')}
+            </th>
+            ))} 
+        </tr>
+      ))}    
+      </thead>
+      <tbody { ...getTableBodyProps() }>
+      { rows.map((row) => {
+        prepareRow(row)
+        return (
+          <tr>
+            <td></td>
+        </tr>
+        )
+      }) }
+        
+      </tbody>
+    </table>
+  )
+}
+
+export default BasicTable
